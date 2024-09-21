3bfe2274749f0ad53292a96506a5f52e908788b9
Aleksandr Ignatyev <vigntom@gmail.com>
2024-09-21 12:47:42 +0300

    Подготовил папку для ДЗ 15.2

---
+++ hw-15.2/infra/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      version = ">=0.100"
+    }
+  }
+  required_version = ">=0.13"
+}
+
+provider "yandex" {
+  token     = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.default_zone
+}
